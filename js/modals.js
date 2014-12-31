28fbc3635369bfcf8f7bdc60ae76428557a85109
k9101 <kris9101@gmail.com>
2014-12-30 21:56:59 -0500

    Vex test

---
+++ js/modals.js
@@ -0,0 +1,14 @@
+vex.defaultOptions.className = 'vex-theme-plain';
+
+/*vex.dialog.open({
+
+	message:"Send an email to Kris",
+	content: "",
+	
+			
+});*/
+
+$('#contact').click(function(){
+	vex.dialog.alert("This is a test");
+
+});
